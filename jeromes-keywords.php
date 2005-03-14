a5afd0b16c8fea4310b9af4b863f9d0e078933f4
darkcanuck <darkcanuck@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-03-14 00:57:59 +0000

    Updated to version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/jeromes-keywords/trunk@1127 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jeromes-keywords.php
+++ jeromes-keywords.php
@@ -1,15 +1,14 @@
 <?php
-
 /*
 Plugin Name: Jerome's Keywords
 Plugin URI: http://vapourtrails.ca/wp-keywords
-Version: 1.3
+Version: 1.4
 Description: Allows keywords to be associated with each post.  These keywords can be used for page meta tags, included in posts for site searching or linked like Technorati tags.
 Author: Jerome Lavigne
 Author URI: http://vapourtrails.ca
 */
 
-/*	Copyright 2005  Jerome Lavigne  (email : jerome@vapourtrails.ca)
+/*	Copyright 2005  Jerome Lavigne  (email : darkcanuck@vapourtrails.ca)
 	
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -30,9 +29,22 @@ Author URI: http://vapourtrails.ca
 	Special thanks to Stephanie Booth (http://climbtothestars.org), whose excellent "Bunny's Technorati Tags" plugin
 	was a great learning resource.  Her plugin is responsible for my use of the get/add/delete_post_meta() functions
 	rather than trying to access the database directly.
+	
+	Thanks also to Dave Metzener, Mark Eckenrode, Dan Taarin and others who have provided feedback, spotted bugs,
+	and suggested improvements.
 */
 
 /* ChangeLog:
+
+13-Mar-2005:  Version 1.4
+		- Added ability to automatically generate .htaccess rewrite rules for keyword searches.
+			- Can be turned off with new KEYWORDS_REWRITERULES flag.
+			- Necessary for sites that use /index.php/*%blah% style permalinks
+			- Thanks to Dave Metzener for finding the original bug and beta testing the fix.
+		- Added formatting parameters to the_keywords() and get_the_keywords() to allow more control over the output.
+		- Fixed XHTML validation bug:  added a space prior to title attribute in keyword links.
+		- Fixed keyword link encoding for links that include '/' (now left as-is rather than encoded)
+		- Temporary fix to prevent conflicts with mini-posts plugin:  removes mini-post's filters when a tag search is performed.
 
 1-Mar-2005:  Version 1.3
 		- Added ability to do site keyword searches.  This now the default keyword link behaviour.
@@ -63,6 +75,11 @@ Template Tags - you can use the following php template tags to insert the keywor
 			Outputs a comma-separated list of all categories & keywords on the current page.  You can use this
 			to add a keyword meta tag to your page's title block:
 				<meta name="keywords" content="<?php the_keywords(); ?>" />
+
+			This function can take three optional parameters:
+				before (default = blank) - text/html to insert before each keyword
+				after (default = blank) - text/html to insert after each keyword
+				separator (default = ",") - text/html to insert between keywords
 			get_the_keywords() is a non-echoing version
 
 	the_post_keywords() - must be used inside the loop
@@ -100,9 +117,13 @@ Template Tags - you can use the following php template tags to insert the keywor
 			get_the_search_keytag() is a non-echoing version
 
 
-Rewrite Rules - To use the tag search feature with your mod_rewrite rules (assuming you are using permalinks - ignore this if not) you should
-			add the following to your .htaccess file, preferably below the "# END WordPress" line:
+Rewrite Rules - The plugin can generate new tag search rewrite rules automatically.  You need to 
+			re-save your permalinks settings (Options -> Permalinks) for this to occur.
+			If your .htaccess file cannot be written to by WordPress, add the following to your 
+			.htaccess file to use the tag search feature, preferably below the "# END WordPress" line:
 
+RewriteRule ^tag/(.+)/feed/(feed|rdf|rss|rss2|atom)/?$ /index.php?tag=$1&feed=$2 [QSA,L]
+RewriteRule ^tag/(.+)/(feed|rdf|rss|rss2|atom)/?$ /index.php?tag=$1&feed=$2 [QSA,L]
 RewriteRule ^tag/(.+)/page/?([0-9]{1,})/?$ /index.php?tag=$1&paged=$2 [QSA,L]
 RewriteRule ^tag/(.+)/?$ /index.php?tag=$1 [QSA,L]
 
@@ -114,15 +135,19 @@ define('KEYWORDS_META', 'keywords');							// post meta key used in the wp datab
 define('KEYWORDS_TECHNORATI', 'http://technorati.com/tag');		// Technorati link to use if local search is false
 define('KEYWORDS_ATOMTAGSON', '1');								// flag to add tags to Atom feed (required for Technorati)
 define('KEYWORDS_QUERYVAR', 'tag');								// get/post variable name for querying tag/keyword from WP
-define('KEYWORDS_TAGURL', 'tag');								// URL to use when querying tags (need to add rewrite rules for this)
+define('KEYWORDS_TAGURL', 'tag');								// URL to use when querying tags
 define('KEYWORDS_TEMPLATE', 'keywords.php');					// template file to use for displaying tag queries
 define('KEYWORDS_SEARCHURL', 'search');							// local search URL (from mod_rewrite rules)
+define('KEYWORDS_REWRITERULES', '1');							// flag to determine if plugin can change WP rewrite rules
 
 /* Shouldn't need to change this - can set to 0 if you want to force permalinks off */
-if (get_settings('permalink_structure'))
-	define('KEYWORDS_REWRITEON',  '1');
-else
-	define('KEYWORDS_REWRITEON',  '0');
+if (isset($wp_rewrite) && $wp_rewrite->using_permalinks()) {
+	define('KEYWORDS_REWRITEON', '1');							// nice permalinks, yes please!
+	define('KEYWORDS_LINKBASE', $wp_rewrite->root);				// set to "index.php/" if using that style
+} else {
+	define('KEYWORDS_REWRITEON', '0');							// old school links
+	define('KEYWORDS_LINKBASE', '');							// don't need this
+}
 
 /* use in the loop*/
 function get_the_post_keywords($include_cats=true) {
@@ -179,10 +204,10 @@ function get_the_post_keytags($include_cats=false, $localsearch="tag", $linktitl
 		foreach($categories as $category) {
 			$keyword = $category->cat_name;
 			if ($linkmode == 'technorati')
-				$taglink = KEYWORDS_TECHNORATI . "/" . urlencode($keyword);
+				$taglink = KEYWORDS_TECHNORATI . "/" . str_replace('%2F', '/', urlencode($keyword));
 			else
 				$taglink = get_category_link($category->category_id);
-			$tagtitle = empty($linktitle) ? "" : "title=\"$linktitle $keyword\"";
+			$tagtitle = empty($linktitle) ? "" : " title=\"$linktitle $keyword\"";
 			
 			if (!empty($output))
 				$output .= ", ";
@@ -202,21 +227,23 @@ function get_the_post_keytags($include_cats=false, $localsearch="tag", $linktitl
 				switch ($linkmode) {
 					case 'tag':
 						if (KEYWORDS_REWRITEON)
-							$taglink = get_settings('home') . "/" . KEYWORDS_TAGURL . "/" . urlencode($keyword);
+							$taglink = get_settings('home') . '/' . KEYWORDS_LINKBASE . KEYWORDS_TAGURL . 
+										'/' . str_replace('%2F', '/', urlencode($keyword));
 						else
 							$taglink = get_settings('home') . "/?" . KEYWORDS_TAGURL .  "=" . urlencode($keyword);
 						break;
 					case 'technorati':
-						$taglink = KEYWORDS_TECHNORATI . "/" . urlencode($keyword);
+						$taglink = KEYWORDS_TECHNORATI . "/" . str_replace('%2F', '/', urlencode($keyword));
 						break;
 					case 'search':
 						if (KEYWORDS_REWRITEON)
-							$taglink = get_settings('home') . "/" . KEYWORDS_SEARCHURL . "/" . urlencode($keyword);
+							$taglink = get_settings('home') . '/' . KEYWORDS_LINKBASE . KEYWORDS_SEARCHURL . 
+										'/' . str_replace('%2F', '/', urlencode($keyword));
 						else
-							$taglink = get_settings('home') . "/?s=" . urlencode($keyword) . "&submit=Search";
+							$taglink = get_settings('home') . '/?s=' . urlencode($keyword) . '&submit=Search';
 						break;
 				}
-				$tagtitle = empty($linktitle) ? "" : "title=\"$linktitle $keyword\"";
+				$tagtitle = empty($linktitle) ? "" : " title=\"$linktitle $keyword\"";
 				
 				if (!empty($output))
 					$output .= ", ";
@@ -238,7 +265,7 @@ function the_post_keytags($include_cats=false, $localsearch=true, $linktitle=fal
 }
 
 /* works outside the loop*/
-function get_the_keywords() {
+function get_the_keywords($before='', $after='', $separator=',') {
 	global $cache_categories, $post_meta_cache;
 	
 	$keywords = "";
@@ -256,8 +283,8 @@ function get_the_keywords() {
 	if (isset($keywordarray)) {
 		foreach($keywordarray as $key => $count) {
 			if (!empty($keywords))
-				$keywords .= ",";
-			$keywords .= $key;
+				$keywords .= $separator;
+			$keywords .= $before . $key . $after;
 		}
 	}
 	
@@ -265,8 +292,8 @@ function get_the_keywords() {
 }
 
 /* works outside the loop */
-function the_keywords() {
-	echo get_the_keywords();
+function the_keywords($before='', $after='', $separator=',') {
+	echo get_the_keywords($before, $after, $separator);
 }
 
 function is_keyword() {
@@ -298,6 +325,10 @@ add_filter('save_post', 'keywords_update');
 /* for keyword/tag queries */
 add_filter('query_vars', 'keywords_addQueryVar');
 add_action('parse_query', 'keywords_parseQuery');
+
+/* generate rewrite rules for above queries */
+if (KEYWORDS_REWRITEON && KEYWORDS_REWRITERULES)
+	add_filter('search_rewrite_rules', 'keywords_createRewriteRules');
 
 /* Atom feed */
 if (KEYWORDS_ATOMTAGSON) {
@@ -374,6 +405,10 @@ function keywords_parseQuery() {
 		$wp_query->is_search = false;
 		$wp_query->is_home = false;
 
+		// mini-posts plugin doesn't play nice with this plugin
+        remove_filter('posts_where', 'mini_posts_where');
+        remove_filter('posts_join', 'mini_posts_join');
+
 		add_filter('posts_where', 'keywords_postsWhere');
 		add_filter('posts_join', 'keywords_postsJoin');
 		add_action('template_redirect', 'keywords_includeTemplate');
@@ -393,7 +428,6 @@ function keywords_postsJoin($join) {
 	return ($join);
 }
 
-			
 function keywords_includeTemplate() {
 
 	if (is_keyword()) {
@@ -414,5 +448,19 @@ function keywords_includeTemplate() {
 	return;
 }
 
+function keywords_createRewriteRules($rewrite) {
+	global $wp_rewrite;
+	
+	// add rewrite tokens
+	$keytag_token = '%' . KEYWORDS_QUERYVAR . '%';
+	$wp_rewrite->rewritecode[] = $keytag_token;
+	$wp_rewrite->rewritereplace[] = '(.+)';
+	$wp_rewrite->queryreplace[] = KEYWORDS_QUERYVAR . '=';
+	
+	$keywords_structure = $wp_rewrite->root . KEYWORDS_QUERYVAR . "/$keytag_token";
+	$keywords_rewrite = $wp_rewrite->generate_rewrite_rules($keywords_structure);
+	
+	return ( $rewrite + $keywords_rewrite );
+}
 
 ?>
